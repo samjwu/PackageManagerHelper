65422c76b1638c6593cc08fbb06bd5d89d5c8d1a
Sam Wu <22262939+samjwu@users.noreply.github.com>
2025-04-09 19:42:29 -0600

    add search fxn

--- apt_helper.sh
+++ apt_helper.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+search_package() {
+    local package_name=$1
+
+    package_info=$(apt search "$package_name" | grep -A 3 "^$package_name")
+
+    echo "$package_info"
+}
+
 if [ -z "$1" ]; then
     echo "Usage: $0 <package-name>"
     exit 1
 fi
+
+search_package "$1"
